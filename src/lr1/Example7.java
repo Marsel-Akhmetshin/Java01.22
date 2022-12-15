d72a35d303a96c5d3fb30b4f36f7404124e371dd
Marsel-Akhmetshin <khlf902@gmail.com>
2022-12-15 23:18:42 +0500

    Initial commit

---
+++ src/lr1/Example7.java
@@ -0,0 +1,22 @@
+package lr1;
+
+import java.util.Scanner;
+
+public class Example7 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Имя: ");
+        String name = in.nextLine();
+
+
+        System.out.println("Возраст: ");
+        int лет = in.nextInt();
+
+        System.out.println("Имя: " +name);
+        System.out.println("Возраст: " +лет);
+        in.close();
+
+    }
+}
