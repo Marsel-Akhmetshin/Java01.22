b1037b113e16f6a940d7ccc0decc4465313c325a
Marsel-Akhmetshin <khlf902@gmail.com>
2022-12-17 19:45:52 +0500

    Initial commit

--- src/lr2/Example1.java
+++ src/lr2/Example1.java
@@ -1,4 +1,20 @@
 package lr2;
 
+import java.util.Scanner;
+
 public class Example1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Проверим, делится ли число на 3 без остатка: ");
+        System.out.println("Введите число: ");
+        int A = in.nextInt();
+        int B = 3;
+        int C = A % B;
+        if(C == 0) {
+            System.out.println("Число делится на 3 без остатка");
+        }
+        else {System.out.println("Число не делится на 3 без остатка");
+            in.close();
+        }
+    }
 }
